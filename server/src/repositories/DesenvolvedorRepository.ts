1d5263b1081bae6d34ed7b8622ad5da3e38cae37
bfsgr <bfsgr.code@protonmail.com>
2021-07-02 09:04:59 -0300

    remove unused methods

--- server/src/repositories/DesenvolvedorRepository.ts
+++ server/src/repositories/DesenvolvedorRepository.ts
@@ -1,33 +1,8 @@
-import { EntityRepository, Like, Repository } from "typeorm";
+import { EntityRepository, Repository } from "typeorm";
 import { Desenvolvedor } from "../entities/Desenvolvedor";
 
 @EntityRepository(Desenvolvedor)
 export class DesenvolvedorRepository extends Repository<Desenvolvedor> {
-  async listarTodosPaginado(offset: number, limit: number) {
-    const query = this.createQueryBuilder("desenvolvedor")
-      .offset(offset)
-      .take(limit);
-
-    return await query.getManyAndCount();
-  }
-
-  async listarDesenvolvedorPorNome(nome: string) {
-    return this.find({ nome: Like(nome) });
-  }
-
-  async buscarDesenvolvedoresPorNomePaginado(
-    nome: string,
-    offset: number,
-    limit: number
-  ) {
-    const query = this.createQueryBuilder("desenvolvedor")
-      .where("desenvolvedor.nome LIKE :nome", { nome: `%${nome}%` })
-      .offset(offset)
-      .take(limit);
-
-    return await query.getManyAndCount();
-  }
-
   async listarComBuscaEPaginacao(nome?: string, page?: number, limit?: number) {
     let query = this.createQueryBuilder("desenvolvedor");
 
