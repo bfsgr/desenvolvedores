b437d2c1e8ac5a93a507c2c2442524529b7bbe18
bfsgr <bfsgr.code@protonmail.com>
2021-07-09 01:41:28 -0300

    change port and add cors

--- server/src/api.ts
+++ server/src/api.ts
@@ -5,12 +5,23 @@ import express, { Express } from "express";
 import { errorHandler } from "./middleware/ErrorHandler";
 import { router } from "./routes";
 import { Server } from "http";
+import cors from "cors";
 
 export class API {
   app: Express;
 
   constructor() {
     this.app = express();
+    this.app.use(
+      cors({
+        exposedHeaders: [
+          "X-Total-Count",
+          "X-Total-Pages",
+          "X-Page-Items",
+          "X-Current-Page",
+        ],
+      })
+    );
     this.app.use(express.json());
     this.app.use(router);
     this.app.use(errorHandler);
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,4 +2,4 @@ import { API } from "./api";
 
 const server = new API();
 
-server.listen(3000, () => console.log("API ONLINE"));
+server.listen(5000, () => console.log("API ONLINE"));
