d332573f494c39cb51fe27d0f2a77f2adb550d68
bfsgr <bfsgr.code@protonmail.com>
2021-07-09 01:43:17 -0300

    gen migration

---
+++ server/src/database/migrations/1625772195015-GenModel.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class GenModel1625772195015 implements MigrationInterface {
+    name = 'GenModel1625772195015'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "desenvolvedor" ("id" SERIAL NOT NULL, "nome" character varying(80) NOT NULL, "sexo" character varying(1) NOT NULL, "hobby" character varying(100) NOT NULL, "data_nascimento" TIMESTAMP NOT NULL, CONSTRAINT "PK_caed89da6970e6d035f4bacea87" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "desenvolvedor"`);
+    }
+
+}
